848c4bcc085e51542f569309b7f86cf2ec5e1fc0
Pravachan Patra <pravachanpatra@gmail.com>
2025-08-24 10:42:09 -0500

    load raw data as pandas df and enforce col datatypes, feature pipeline

---
+++ backend/ml/pipelines/feature_pipeline.py
@@ -0,0 +1,67 @@
+# FEATURE PIPELINE: transforms raw data into ready-to-train features/labels saved in feature store
+import pandas as pd
+import numpy as np
+import json
+from dotenv import load_dotenv
+load_dotenv()
+
+
+# Global Variables in pipeline
+RAW_DATA_COLUMNS = ["image_url", "patient_details", "disease_classification_vector", "report"]
+
+# Helper functions
+def print_clean_df(df, num_rows=5, display_head=True, str_length=15):
+    display_df = df.copy()
+    # Get the columns that contain string data
+    str_cols = display_df.select_dtypes(include=['object']).columns
+    # Truncate string columns
+    for col in str_cols:
+        display_df[col] = display_df[col].astype(str).str.slice(0, str_length) + \
+                          display_df[col].astype(str).str.len().gt(str_length).apply(lambda x: '...' if x else '')
+    # Print the head or tail
+    if display_head:
+        print(display_df.head(num_rows).to_string())
+    else:
+        print(display_df.tail(num_rows).to_string())
+
+
+
+# Enforces data-types in raw-data-df
+def enforce_raw_data_columns(df):
+    # make sure image-url input col, patient-detials input-col, and report output col are of type string in dataframe
+    df["image_url"] = df["image_url"].astype(str)
+    df["patient_details"] = df["patient_details"].astype(str)
+    df["report"] = df["report"].astype(str)
+
+    def _to_float_vector(x):
+        if isinstance(x, (list, tuple, np.ndarray)):
+            arr = np.asarray(x, dtype=float)
+        else:
+            arr = np.asarray(json.loads(str(x)), dtype=float)
+        if arr.size != 13: # for 13 disease classes
+            raise ValueError(f"Expected vector of length 14, got {arr.size}")
+        return arr
+
+    # make sure disease-class-vec [1,0,0,1,0,1,...] is of type float vector
+    df["disease_classification_vector"] = df["disease_classification_vector"].map(_to_float_vector)
+    return df
+
+# Load in raw data from S3-bucket given s3-bucket-url, return pandas dataframe of raw data
+def load_raw_data(s3_url):
+    # load parquet raw-data-file from s3-bucket using s3-url of where the parquert file is located, and convert it into a pandas-dataframe
+    raw_data_df = pd.read_parquet(s3_url, columns=RAW_DATA_COLUMNS, storage_options={}, engine="pyarrow")
+    # make sure columns in raw data are correct type
+    raw_data_df = enforce_raw_data_columns(raw_data_df)
+    return raw_data_df
+
+
+
+def tests():
+    raw_data_df = load_raw_data("s3://medical-ml-proj-bucket/raw_data/dataset.parquet")       # make sure this url of where the raw data is in s3 is correct
+
+    print("\n---------LOAD SHOW RAW DATA:---------")
+    print_clean_df(raw_data_df, num_rows=3)
+
+
+tests()
+
