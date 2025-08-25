3346de233f513160c0f236df642422078ca9544f
Pravachan Patra <pravachanpatra@gmail.com>
2025-08-25 17:06:34 -0500

    get image from s3-bucket as bytes object in training pipline

--- backend/ml/pipelines/training_pipeline.py
+++ backend/ml/pipelines/training_pipeline.py
@@ -4,7 +4,14 @@ import pandas as pd
 import numpy as np
 import json
 from helper import print_clean_df
+import torch
 import warnings
+# import tensorflow as tf
+import io
+import os
+import boto3
+from dotenv import load_dotenv
+load_dotenv()
 warnings.filterwarnings("ignore", category=UserWarning)     # supress hopsworks warings for now caution!
 
 
@@ -14,8 +21,8 @@ HOPS_FEATURE_GROUP_NAME="cxr_features"      # name of feature-group in feature-s
 VERSION = 1
 INPUT_COLS = []
 OUTPUT_COLS = []
+s3 = boto3.client("s3")
 
-# Helper functions
 
 
 # Get features & labels from hopsworks feature-group in feature store
@@ -39,6 +46,19 @@ def load_features_labels_from_feature_store():
 
     return features_labels_df
 
+# --------Some helper functions to get images from S3-bucket--------
+def get_image_from_s3(bucket, key):  # gets the image from s3 given bucket-name & objects key
+    bio = io.BytesIO()                      # creates in-memory binary stream which is an object that behaves as a file, but doesnt interact with file system
+    s3.download_fileobj(bucket, key, bio)   # downloads object from s3 as a file like object, given bucket and key of object
+    return bio.getvalue()                   # returns the entire contents of in-mem buffer as a bytes object, this is the image file as a bytes object retrived fomr s3
+
+# given a s3-url of an image gets the bucket and key of where that image lives, so we can get that image file
+def parse_s3_url(url):
+    assert url.startswith("s3://")
+    no_schema = url[5:]
+    bucket, key = no_schema.split("/", 1)
+    return bucket, key
+
 # Transform them to be inputted into models
 
 # Create models
@@ -50,8 +70,16 @@ def load_features_labels_from_feature_store():
 
 def training_tests():
     print("----------LOAD FEATURES/LABELS FROM FEATURE STORE HOPSWORKS---------")
-    features_labels_df = load_features_labels_from_feature_store()
-    print_clean_df(features_labels_df, num_rows=10)
+    # features_labels_df = load_features_labels_from_feature_store()
+    # print_clean_df(features_labels_df, num_rows=10)
+
+
+    print("---------GET IMAGE FROM S3 AS BYTES OBJECT---------")
+    s3_image_url_example = "s3://medical-ml-proj-bucket/chest-x-ray-images/cc4ff72b-a3adf0.jpg"
+    bucket, key = parse_s3_url(s3_image_url_example)
+    image_bytes_obj = get_image_from_s3(bucket, key)
+    print(f"image bytes object: {image_bytes_obj[0:5]}")
+
 
 
 training_tests()
