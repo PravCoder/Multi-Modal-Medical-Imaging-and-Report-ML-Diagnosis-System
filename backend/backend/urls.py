c286d66eb381f2e454fa54b0bd0c74ec88c18b83
Pravachan Patra <pravachanpatra@gmail.com>
2025-09-12 11:08:35 -0500

    remove catch call in urls.py, update nginz config o serve index.html

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),  # inlcude urls in api-app
 
-    re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
+    # re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
 
 
 ]
