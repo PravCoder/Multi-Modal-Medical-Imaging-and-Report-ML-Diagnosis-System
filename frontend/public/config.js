97e4fc421b657979c014e0a07fdf9187f5ecb77d
Pravachan Patra <pravachanpatra@gmail.com>
2025-09-13 14:42:14 -0500

    choero deployment: changes to connect backend & frontend

---
+++ frontend/public/config.js
@@ -0,0 +1,6 @@
+// window.configs = {
+//     apiUrl: '/choreo-apis/multimodalmedicalapp/backend/v1',
+//     consumerKey: "ncKfoD7EyQOP7BhwJsBjBJfaPRzJ", // use the one Choreo gave
+//     consumerSecret: 'k2jcOxUE6jawu3JOwUZcoMzMdi5s',      // the secret from Choreo
+//     tokenUrl: 'https://61677c98-9037-4034-8b24-9e4197fea894-dev.e1-us-east-azure.choreosts.dev/oauth2/token',
+// };
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-// If your API is on a different origin/port, set it here, e.g.:
-// const API_BASE = "http://localhost:8000";
-
-const apiUrl = "/choreo-apis/multimodalmedicalapp/backend/v1";
+const apiUrl = import.meta.env.VITE_API_URL
+  ? import.meta.env.VITE_API_URL
+  : "/choreo-apis/multimodalmedicalapp/backend/v1";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+  baseURL: apiUrl,
+  // no need for Authorization header in frontend
 });
