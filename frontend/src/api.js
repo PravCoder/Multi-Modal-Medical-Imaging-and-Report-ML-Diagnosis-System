920e5c0f795aed06d6ad1c320e01291e84124fa9
Pravachan Patra <pravachanpatra@gmail.com>
2025-09-13 14:52:18 -0500

    choreo deployment: apiUrl for production, frontend connection to back

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
-const apiUrl = import.meta.env.VITE_API_URL
-  ? import.meta.env.VITE_API_URL
-  : "/choreo-apis/multimodalmedicalapp/backend/v1";
+// for locally working:
+// const apiUrl = import.meta.env.VITE_API_URL
+//   ? import.meta.env.VITE_API_URL
+//   : "/choreo-apis/multimodalmedicalapp/backend/v1";
+
+
+// for production working:
+const apiUrl = window?.configs?.apiUrl ? window.configs.apiUrl : "/";
 
 export const api = axios.create({
   baseURL: apiUrl,
